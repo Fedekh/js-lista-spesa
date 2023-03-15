72e9d372bf5160b38ead37f7ae999601addd2819
Fedekh <federicocet@gmail.com>
2023-03-15 13:37:19 +0100

    add scaffolding project

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// Descrizione:
+
+// Data una lista della spesa, stampare sulla pagina (anche brutalmente, basta che si vedano) gli elementi della lista individualmente con un ciclo while.
+// Rivediamo l’esercizio visto insieme, ma questa volta con il ciclo while al posto del ciclo for.
+//  Facciamo attenzione a non dimenticare tutte le differenze implementative che il ciclo while ci impone, in particolare alla variabile di indice.
+// Consigli:
+// - Ricordiamoci di inizializzare la variabile di contatore prima dell'inizio del ciclo while
+// - Ricordiamoci di incrementare il contatore, o comunque di inserire una condizione di uscita, all'interno ciclo while
+
+
+// introduciamo come variabile, data per default, un array che rappresenta la lista della spesa
+
+const shoppingArray = ["mele", "pesche", "biscotti", "detersivi", "pasta", "acqua", "coca-cola"];
+console.log(shoppingArray, typeof shoppingArray);
