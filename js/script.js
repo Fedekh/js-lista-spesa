ec46896c8b83bd380dfd651001191587659ec6dc
Fedekh <federicocet@gmail.com>
2023-03-15 14:08:00 +0100

    conclude exercise, milestone 1, after positive feedback, i continue with ipotetic milestone 2

--- js/script.js
+++ js/script.js
@@ -7,8 +7,38 @@
 // - Ricordiamoci di inizializzare la variabile di contatore prima dell'inizio del ciclo while
 // - Ricordiamoci di incrementare il contatore, o comunque di inserire una condizione di uscita, all'interno ciclo while
 
+// ############################################################################################################
 
 // introduciamo come variabile, data per default, un array che rappresenta la lista della spesa
 
 const shoppingArray = ["mele", "pesche", "biscotti", "detersivi", "pasta", "acqua", "coca-cola"];
 console.log(shoppingArray, typeof shoppingArray);
+
+// voglio provare a usare questo tipo di manipolazione, creando elementi con js, in questo caso UL
+const listElem = document.createElement("ul");
+console.log(listElem);
+
+//creo una variabile legata a un elemento presente gia in html, in questo caso .container, al cui Interno, usando giusta sintassi, inserisco, 
+//dopo vari elementi gia presenti in HTML, ciò che ho creato sopra
+const container = document.querySelector(".container");
+container.append(listElem);
+
+
+// inizializzo sia il contatore per il ciclo while e sia una variabile di accumulo che mi servirà sia dentro che fuori while
+let listItem = "";
+let i = 0;
+
+
+while (i < shoppingArray.length) {
+    // console.log (shoppingArray[i]);
+    // la lista listItem è formata dalla concatenazione di stringhe, opportunamente scritte con sintassi HTML,
+    // di <li> progressivi dipendenti dall'indice i
+    listItem += `<li class= "text-white">${shoppingArray[i]}</li>`;
+    i++;
+}
+
+// ora introduco questi LI creati dinamicamente con js, usando giusta sintassi, nell'elemento UL sopra creato, dovrebbe uscire tutto in html
+
+listElem.innerHTML = listItem;
+
+
